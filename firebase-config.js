026cf51e9dc536161a51127c247bd1ea3d1de067
lukaspokornydev <lukaspokornydev@gmail.com>
2026-02-08 16:45:18 +0100

    ignore

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyCiD36lqOIrGmO-9z2Zfvw8GLErGFdAz70",
+  authDomain: "movies-tracker-b78dd.firebaseapp.com",
+  projectId: "movies-tracker-b78dd",
+  storageBucket: "movies-tracker-b78dd.firebasestorage.app",
+  messagingSenderId: "235167631216",
+  appId: "1:235167631216:web:7e70ea3379e10cdfbbf1b5"
+};
